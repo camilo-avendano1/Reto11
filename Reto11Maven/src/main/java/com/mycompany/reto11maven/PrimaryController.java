e1158ac51ff65d57a64f6a7b2ef3865bbb44c4d4
camilo-avendano1 <81989221+camilo-avendano1@users.noreply.github.com>
2022-03-25 19:37:10 -0500

    Add files via upload
    completa

--- Reto11Maven/src/main/java/com/mycompany/reto11maven/PrimaryController.java
+++ Reto11Maven/src/main/java/com/mycompany/reto11maven/PrimaryController.java
@@ -6,14 +6,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import static java.util.Collections.list;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -56,12 +59,12 @@ public class PrimaryController implements Initializable {
 
     //creamos la listaobservable que eslo que aceptael table view
     public ObservableList Listar() throws IOException {//el throws es porque lo pide al ser parte de archivo btc qeu hacemos lectura
-                Path ruta = Paths.get("BTC-USD.csv");
+        Path ruta = Paths.get("BTC-USD.csv");
         List<String> documento = Files.readAllLines(ruta);
         ObservableList<archivoBTC> list = FXCollections.observableArrayList(
                 new archivoBTC(0)); // agregamos el primer archivo pa qeu pueda detectar la lisa
-        for(int i = 1; i < documento.size(); i++){ // en un bucle agregamos el resto elementos creando n archivos btc, donde cada uno se le asigna su respectiva linea
-            archivoBTC archivo =  new archivoBTC(i);
+        for (int i = 1; i < documento.size(); i++) { // en un bucle agregamos el resto elementos creando n archivos btc, donde cada uno se le asigna su respectiva linea
+            archivoBTC archivo = new archivoBTC(i);
             list.add(archivo); // lo agregamos a la ObserveList
         }
         return list;
@@ -82,6 +85,118 @@ public class PrimaryController implements Initializable {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    @FXML
+    private void alertPromedio(ActionEvent event) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setTitle("Promedio");
+        alert.setHeaderText("Promedio del BTC durante 2022");
+        alert.setContentText(String.valueOf(MediaBTC("BTC-USD.csv")));
+        alert.showAndWait();
+
+    }
+
+    @FXML
+    private void alertMayor(ActionEvent event) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setTitle("Mayor");
+        alert.setHeaderText("Mayor Precio del BTC durante 2022");
+        alert.setContentText(String.valueOf(PrecioMasAlto("BTC-USD.csv")));
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void alertMenor(ActionEvent event) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setTitle("Menor");
+        alert.setHeaderText("Menor precio del BTC durante 2022");
+        alert.setContentText(String.valueOf(PrecioMasBajo()));
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void alertDesEstand(ActionEvent event) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setTitle(
+                "Desviacion estandar");
+        alert.setHeaderText(
+                "Desviacion Estandar del BTC durante 2022");
+        alert.setContentText(
+                String.valueOf(DesviacionEstandar("BTC-USD.csv")));
+        alert.showAndWait();
+    }
+
+    //metodos a usar para sacar los botones
+    public static Double PrecioMasBajo() throws IOException {
+        Path ruta = Paths.get("BTC-USD.csv");
+        String[] linea;
+        ArrayList<Double> Minimos = new ArrayList();
+
+        List<String> documento = Files.readAllLines(ruta);
+        for (int i = 1; i < documento.size(); i++) {
+            linea = documento.get(i).split(",");
+            double minimoDeLaLinea = Double.parseDouble(linea[2]);
+
+            Minimos.add(minimoDeLaLinea);
+        }
+        // aprovecahmos el collecions que nos saca el valor minimo de una lista
+        return Collections.min(Minimos);
+    }
+
+    public static double MediaBTC(String ruta1) throws IOException {
+        Path ruta = Paths.get(ruta1);
+        String[] linea;
+        double media = 0;
+        List<String> documento = Files.readAllLines(ruta);
+
+        for (int i = 1; i < documento.size(); i++) {
+            linea = documento.get(i).split(",");
+            double valor = Double.parseDouble(linea[1]);
+            media += valor;
+        }
+        return (media / documento.size());
+
+    }
+
+    public static double DesviacionEstandar(String Ruta) throws IOException {
+        Path ruta = Paths.get(Ruta);
+        String[] linea;
+        double desviacion;
+        double varianza = 0;
+        List<String> documento = Files.readAllLines(ruta);
+        for (int i = 1; i < documento.size(); i++) {
+            linea = documento.get(i).split(",");
+            double rango;
+            rango = Math.pow(Double.parseDouble(linea[1]) - MediaBTC(Ruta), 2f);
+            varianza = varianza + rango;
+        }
+        varianza = varianza / documento.size();
+        desviacion = Math.sqrt(varianza);
+        return desviacion;
+    }
+
+    public static double PrecioMasAlto(String ruta1) throws IOException {
+
+        Path ruta = Paths.get(ruta1);
+        String[] linea;
+
+        ArrayList<Double> Mayores = new ArrayList();
+
+        List<String> documento = Files.readAllLines(ruta);
+        for (int i = 1; i < documento.size(); i++) {
+            linea = documento.get(i).split(",");
+            double high = Double.parseDouble(linea[2]);
+
+            Mayores.add(high);
+        }
+        // aprovecahmos el collecions que nos saca el valor maximo de una lista
+        return Collections.max(Mayores);
+
     }
 
 }
--- Reto11Maven/src/main/java/com/mycompany/reto11maven/SecondaryController.java
+++ Reto11Maven/src/main/java/com/mycompany/reto11maven/SecondaryController.java
@@ -1,16 +1,72 @@
 package com.mycompany.reto11maven;
 
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 
-public class SecondaryController {
+public class SecondaryController implements Initializable {
 
     @FXML
     private Button secondaryButton;
+    @FXML
+    private LineChart<?, ?> lineChart;
 
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
+
+    public ObservableList Listar() throws IOException {//el throws es porque lo pide al ser parte de archivo btc qeu hacemos lectura
+        Path ruta = Paths.get("BTC-USD.csv");
+        List<String> documento = Files.readAllLines(ruta);
+        ObservableList<archivoBTC> list = FXCollections.observableArrayList(
+                new archivoBTC(0)); // agregamos el primer archivo pa qeu pueda detectar la lisa
+        for (int i = 1; i < documento.size(); i++) { // en un bucle agregamos el resto elementos creando n archivos btc, donde cada uno se le asigna su respectiva linea
+            archivoBTC archivo = new archivoBTC(i);
+            list.add(archivo); // lo agregamos a la ObserveList
+        }
+        return list;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        lineChart.setTitle("Precio Bitcoin 2022");
+
+        XYChart.Series series = new XYChart.Series();
+
+        series.setName("Fecha");
+
+        Path ruta = Paths.get("BTC-USD.csv");
+        List<String> documento = null;
+        try {
+            documento = Files.readAllLines(ruta);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        for (int i = 0; i < documento.size(); i = i + 10) { // lo hacemos saltando de 10 en 10 para que no quede muy apeñuscado
+            archivoBTC archivo = null;
+            try {
+                archivo = new archivoBTC(i);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            series.getData().add(new XYChart.Data(archivo.getFechas(), Double.parseDouble(archivo.getAlto())));  //agregamos cada dato del archivo (fecha, precio) a una nueva lista del tipo serie
+        }
+
+        //agregamos al lineChart
+        lineChart.getData().add(series); //lo agregamos al linechart
+
+    }
+
 }
